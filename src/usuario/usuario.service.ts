67eabbbbfb84d53b53d3612b2f2b0a90111e4f4c
bebetoneto <rbcorreaneto@gmail.com>
2025-06-10 13:32:41 -0300

    merge

--- src/usuario/usuario.service.ts
+++ src/usuario/usuario.service.ts
@@ -1,16 +1,16 @@
 import { ConflictException, Injectable, NotFoundException } from "@nestjs/common";
 import { UsuarioDto } from "./dto/Usuario.dto.js";
 import { PrismaService } from "src/database/prisma.service";
-import { UpdateUsuarioDto } from "./dto/UpdateUsuario.dto.js";
+import * as bcrypt from "bcrypt";
 
 @Injectable()
 export class UsuarioService{
 
-    constructor(private prisma: PrismaService){}
+    constructor (private prisma: PrismaService){}
 
     //funcao para achar usuario pelo id
     private async findUserById(id: number) {
-        const usuario = await this.prisma.usuario.findUnique({where: { id: Number(id) } });
+        const usuario = await this.prisma.usuario.findUnique({ where: { id: Number(id) } });
 
         if (!usuario) {
             throw new NotFoundException(`Usuário com id ${id} não encontrado.`);
@@ -21,6 +21,7 @@ export class UsuarioService{
 
     // --------------------CRUD--------------------//
     
+    //Create
     async create(data: UsuarioDto){
         const usuario = await this.prisma.usuario.findUnique({where : {email : data.email}})
 
@@ -28,19 +29,43 @@ export class UsuarioService{
             throw new ConflictException(`Usuário com ${data.email} ja cadastrado!`)
         }
 
-        return this.prisma.usuario.create({ data })
+        const hashedPassword = await bcrypt.hash(data.senha,10)
+
+        return this.prisma.usuario.create({ data : { ...data,senha: hashedPassword} })
     }
 
+    async findByEmail(email:string){
+        const usuario = await this.prisma.usuario.findUnique({where : {email : email}})
+
+        if(!usuario){
+            return null
+        }
+
+        return usuario
+    }
+
+    //Read
     async findAll(){
-        return this.prisma.usuario.findMany()
+        return this.prisma.usuario.findMany( {select : {
+            id : true,
+            nome: true,
+            email : true,
+            createdAt : true,
+            updatedAt : true
+          //  avaliacoes: true,
+          //  comentarios : true
+        },})
     }
 
+    //ReadOne
     async findOneID(id:number){
       return this.findUserById(id)
     }
 
-    async update(id:number, data: UpdateUsuarioDto){
+    //Update
+    async update(id:number, data: UsuarioDto){
         const usuarioExistente = await this.findOneID(id)
+        
         if(usuarioExistente){
             if(data.email){
                 const verificaEmail = await this.prisma.usuario.findUnique({where: {email : data.email}})
@@ -53,6 +78,7 @@ export class UsuarioService{
         return this.prisma.usuario.update({where : {id : Number(id)},data})
     }
 
+    //Delete
     async delete(id:number){
         await this.findUserById(id)
         return this.prisma.usuario.delete({where : { id } })
